d483c098d7f2baba07596f934a17b1f0831b43d9
Montesit00 <marcos.montellanok@gmail.com>
2023-10-24 16:27:52 -0300

    Conexion a bd incompleto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductosModule } from './productos/productos.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductosModule],
+  imports: [ProductosModule, MongooseModule.forRoot('mongodb+srv://monte:monte123@monte.xnfkexl.mongodb.net/progra?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/productos/entities/producto.entity.ts
+++ src/productos/entities/producto.entity.ts
@@ -1,1 +1,18 @@
-export class Producto {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { HydratedDocument } from 'mongoose'
+
+export type productoDocument = HydratedDocument<Producto>;
+
+@Schema()
+export class Producto {
+    @Prop()
+    id: string;
+
+    @Prop()
+    marca: string;
+
+    @Prop()
+    stock: number;
+}
+
+export const ProductoSchema = SchemaFactory.createForClass(Producto)
